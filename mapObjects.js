1783e27752944cec337586b7f5422f52209f130e
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-15 12:33:18 +0530

    added mapobject

---
+++ mapObjects.js
@@ -0,0 +1,14 @@
+function mapObjects(object, callBack) {
+    if (typeof object == undefined || Array.isArray(object) == true || typeof object != 'object') {
+        return []
+    } else {
+
+        if (callBack != undefined) {
+            for (var property in object) {
+                object[property] = callBack(object[property], property)
+            }
+        }
+        return object
+    }
+}
+module.exports = mapObjects;
---
+++ test/testMapObjects.js
@@ -0,0 +1,7 @@
+const values = require("../mapObjects")
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham', halla: { 1: "one" }, gello: function () { return 2 * 3 } };
+
+let callBack = (value,key) => value + 5
+
+const result = values(testObject,callBack);
+console.log(result)
